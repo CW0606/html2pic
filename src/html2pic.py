baefe0545793e775baf698e5703b08a4368ce358
王达武 <wangdawu@okjiaoyu.cn>
2017-02-20 13:59:21 +0800

    update

--- src/html2pic.py
+++ src/html2pic.py
@@ -9,6 +9,7 @@ import time
 import codecs
 import sys
 
+
 def _generate_valid_html(math_express, css_path=None):
     """将匹配成功的代码添加标签"""
     if css_path is None:
@@ -39,10 +40,10 @@ def _crop(div, span, screenshot_path):
     """截图 这儿通过传入一个span获得需要截取部分的长度,传入一个div获得
     对象的高度来保证在截图时不会截取过长同时又不会纵向截取缺失"""
     img = Image.open(screenshot_path)
-    left = int(div.location['x'])
-    top = int(div.location['y'])
-    right = left + span.size['width']
-    bottom = top + div.size['height']
+    left = int(div.location ['x'])
+    top = int(div.location ['y'])
+    right = left + span.size ['width']
+    bottom = top + div.size ['height']
     window = (left, top, right, bottom)
     region = img.crop(window)
     png_name = str(long(time.time() * 10000)) + '.png'
@@ -53,7 +54,6 @@ def _crop(div, span, screenshot_path):
 
 def _draw(driver, html):
     """传入的html代码进行截图"""
-    print(html[0:100])
     html_path = _save_valid_html(html)
     url = "file://" + html_path
     driver.get(url)
@@ -67,6 +67,12 @@ def _draw(driver, html):
     _delete_file(html_path)
     _delete_file(screenshot_path)
     return png_path
+
+
+def _replace_start(data_str, src_str, des_str=u''):
+    if data_str.startswith(src_str):
+        return des_str + data_str[len(src_str):]
+    return data_str
 
 
 def html2pic(rules_path, html_path=None, html_content=None, driver=None):
@@ -83,19 +89,52 @@ def html2pic(rules_path, html_path=None, html_content=None, driver=None):
     html2pics = dict()
     for html in match_htmls:
         print html[0:100]
+        # html = _replace_start(html, u'/>')
+        # html = _replace_start(html, u'A.')
+        # html = _replace_start(html, u'B.')
+        # html = _replace_start(html, u'C.')
+        # html = _replace_start(html, u'D.')
+        # html = _replace_start(html, u'nbsp;')
         html2pics.update({html: _draw(driver, html)})
     return html2pics
 
 
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 3:
-       print("""
-       example: python html2pic rules_path html_path
-       """)
+def test_from_db():
+    import MySQLdb
+    db = MySQLdb.connect(host='172.18.4.81', user='admintest', passwd='dsjw2015',
+                         db='homework', port=3307, charset='utf8')
+    cursor = db.cursor()
+    cursor.execute(
+        """select option_a,option_b,option_c,option_d,title,parse,answer1,
+        answer2 from questions where subjectId=9 limit 0, 10""")
+    data_list = cursor.fetchall()
     driver = webdriver.Chrome()
-    try:
-        print html2pic(rules_path = args[1], html_path=args[2], driver=driver)
-    except Exception as e:
-        print e
+    results = list()
+    for data in data_list:
+        for item in data:
+            if item is None or len(item) == 0:
+                pass
+            else:
+                print(item)
+                result = html2pic("/Users/lovechenao/gitroom/html2pic/"
+                                  "src/okay/rules/chemistry.rule",
+                                  html_content=item,driver=driver)
+                results.append(result)
     driver.close()
+
+
+
+
+if __name__ == '__main__':
+    # args = sys.argv
+    # if len(args) != 3:
+    #     print("""
+    #    example: python html2pic rules_path html_path
+    #    """)
+    # driver = webdriver.Chrome()
+    # try:
+    #     print html2pic(rules_path=args[1], html_path=args[2], driver=driver)
+    # except Exception as e:
+    #     print e
+    # driver.close()
+    test_from_db()
